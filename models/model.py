5a703b7487baf8f0aa922b26b3477eeef816f8ed
Vincent Mousseaux <mousseaux.v@gmail.com>
2025-04-26 13:20:16 +0200

    Feature: Add simple model

---
+++ models/model.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+class Model(nn.Module):
+    def __init__(self, input_size=10, hidden_size=20, output_size=1):
+        super(Model, self).__init__()
+        self.layer1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.layer2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.relu(x)
+        x = self.layer2(x)
+        return x
